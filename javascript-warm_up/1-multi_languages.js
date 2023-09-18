3f3351c9f744acdb959951b37e48d090dec8c372
TechieDrixx <kaydee12000@gmail.com>
2023-09-18 07:37:59 -0700

    script

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+Script prints 3 line :
+-"C is fun"
+-"Python is cool"
+-"JavaScript is amazing"
+
+*/
+
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
