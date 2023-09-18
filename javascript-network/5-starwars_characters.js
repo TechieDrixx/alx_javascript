eec3c05310719706cd26516a898a66d7f107fe6a
TechieDrixx <kaydee12000@gmail.com>
2023-09-18 09:27:21 -0700

    script

---
+++ javascript-network/5-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else {
+        const data = JSON.parse(body);
+        const allcharacters = data.characters;
+
+        for (const char of allcharacters) {
+            request.get(char, (error, response, charbody) => {
+                if (error) {
+                    console.error('Error:', error);
+                } else {
+                    const chardata = JSON.parse(charbody);
+                    console.log(chardata.name);
+                }
+            });
+        }
+    }
+});
